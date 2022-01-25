cbd97325f5594d70986cd01743eaa8dc5e1d9fdb
Madzik <magda_poliwka@op.pl>
2022-01-25 20:46:06 +0000

    Initial commit

---
+++ src/test/java/com/opendata/meteo/MeteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opendata.meteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
