4f43ed58a0a614951b827a8edd71d5c52233429d
magda_poliwka@op.pl <Dublingalway1>
2023-09-14 22:39:59 +0100

    Document add createdAt

--- src/main/java/com/opendata/meteo/application/model/Document.java
+++ src/main/java/com/opendata/meteo/application/model/Document.java
@@ -1,7 +1,11 @@
 package com.opendata.meteo.application.model;
 
+import java.time.LocalDate;
+
 public class Document {
     private String title;
     private String format;
     private byte [] content;
+
+    private LocalDate createdAt;
 }
